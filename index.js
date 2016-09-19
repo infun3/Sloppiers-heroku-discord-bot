f5f14e77a1874f22d1600798bae0250dc84120eb
HiItsMeHaHa <wherron01@gmail.com>
2016-09-19 11:13:30 -0500

    Added color role when user uses color command
    Still needs to set color

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ setInterval(function() {
 	
 }, [1]);
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hello World!');
 })
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("Node app is running at localhost:" + app.get('port'));
 })
